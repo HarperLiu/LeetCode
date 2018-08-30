8a6766e10538bedb49b000079b3b742df5dc6174
HarperLiu <985519597@qq.com>
2018-08-30 15:07:27 +0800

    update word break solution

--- module/src/WordBreak/Solution.java
+++ module/src/WordBreak/Solution.java
@@ -1,10 +1,14 @@
 package WordBreak;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+/*
+这道题仍然是动态规划的题目，我们总结一下动态规划题目的基本思路。首先我们要决定要存储什么历史信息以及用什么数据结构来存储信息。然后是最重要的递推式，就是如从存储的历史信息中得到当前步的结果。最后我们需要考虑的就是起始条件的值。
+接下来我们套用上面的思路来解这道题。首先我们要存储的历史信息res[i]是表示到字符串s的第i个元素为止能不能用字典中的词来表示，
+我们需要一个长度 为n的布尔数组来存储信息。然后假设我们现在拥有res[0,...,i-1]的结果，我们来获得res[i]的表达式。
+思路是对于每个以i为结尾的子 串，看看他是不是在字典里面以及他之前的元素对应的res[j]是不是true，如果都成立，那么res[i]为true
+ */
 public class Solution {
 
         public boolean wordBreak(String s, List<String> wordDict) {
