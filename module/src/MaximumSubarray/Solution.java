e6971f6e5d3a41d76788d6c2dc2e59d018908ec1
HarperLiu <985519597@qq.com>
2018-09-04 00:51:02 +0800

    update maximum subarray

--- module/src/MaximumSubarray/Solution.java
+++ module/src/MaximumSubarray/Solution.java
@@ -4,11 +4,13 @@ package MaximumSubarray;
 如果nums[i,j]是最大的子串
 那么nums[i,k]一定是大于0的，否则nums[k,j]才是最大的子串（前面的和是负数啊我带你岂不是拖了后腿）
 所以从nums[0]遍历数组：
+  把nums[k]加进curSum
+  更新max确保存进去的是当前的最大值
   如果nums[i,k]（即curSum）>0，那么继续加下一个
-  否则清空curSum，从nums[k]开始继续
+  否则清空curSum，从nums[k]开始重新加和
  */
 
-/*
+/*；另一种思路：
 如果nums[i,j]是最大的子串，那么其他的子串只可能是以下2种情况：
   nums[i-k,j]或nums[i,j+k]，即多出来了一部分
   nums[i+k,j]或nums[i,j-k]，即子串少一部分
@@ -18,7 +20,7 @@ package MaximumSubarray;
  */
 public class Solution {
     public int maxSubArray(int[] nums) {
-        /*双指针方法，但是超时了
+        /*双指针方法，超时了
         int max = nums[0];
         for(int i=1;i<=nums.length;i++){//subarray长度
             for(int j=0;j<=nums.length-i;j++){//截取数组开始位置
