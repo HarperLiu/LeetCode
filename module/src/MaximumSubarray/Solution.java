c92718835e2136538d3eac010112473072dfdfc8
HarperLiu <985519597@qq.com>
2018-09-04 00:57:51 +0800

    update solution

--- module/src/MaximumSubarray/Solution.java
+++ module/src/MaximumSubarray/Solution.java
@@ -5,12 +5,11 @@ package MaximumSubarray;
 那么nums[i,k]一定是大于0的，否则nums[k,j]才是最大的子串（前面的和是负数啊我带你岂不是拖了后腿）
 所以从nums[0]遍历数组：
   把nums[k]加进curSum
-  更新max确保存进去的是当前的最大值
-  如果nums[i,k]（即curSum）>0，那么继续加下一个
-  否则清空curSum，从nums[k]开始重新加和
+  如果nums[i,k]（即curSum）<0，那么curSum=0。//解决子串偏大的问题
+  max取curSum和max中较大的 //解决子串偏小的问题
  */
 
-/*；另一种思路：
+/*；通俗一点
 如果nums[i,j]是最大的子串，那么其他的子串只可能是以下2种情况：
   nums[i-k,j]或nums[i,j+k]，即多出来了一部分
   nums[i+k,j]或nums[i,j-k]，即子串少一部分
