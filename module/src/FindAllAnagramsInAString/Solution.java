4312155dce40f0cf835e6ec47276112e9d688020
HarperLiu <985519597@qq.com>
2018-09-05 11:14:03 +0800

    update find all abagrams in s string

--- module/src/FindAllAnagramsInAString/Solution.java
+++ module/src/FindAllAnagramsInAString/Solution.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        ArrayList<Integer>res = new ArrayList<Integer>();
+        ArrayList<Integer> res = new ArrayList<Integer>();
         /*这部分是最后一个用例超时...强行先这样吧
         if(s.contains("abcdefghijklmnopqrstuvwxyz")){
             for(int i=0;i<10063;i++){
@@ -17,19 +17,49 @@ public class Solution {
             return res;
         }*/
         p = sortStr(p);
-        for(int i=0;i<s.length()-p.length()+1;i++){
-            String temp = sortStr(s.substring(i,i+p.length()));
-            if(temp.equals(p)){
+        for (int i = 0; i < s.length() - p.length() + 1; i++) {
+            String temp = sortStr(s.substring(i, i + p.length()));
+            if (temp.equals(p)) {
                 res.add(i);
             }
         }
         return res;
     }
-    public String sortStr(String s){
+
+    public String sortStr(String s) {
         char[] list = s.toCharArray();
         Arrays.sort(list);
         return new String(list);
     }
+
+    /*存储子串和p中各个字母的出现次数，根据次数判断是否一致
+        public List<Integer> findAnagrams(String s, String p) {
+            List<Integer> res = new ArrayList<>();
+            if (p == null || s == null || s.length() < p.length()) return res;
+            int m = s.length(), n = p.length();
+            for (int i = 0; i < m - n + 1; i++) {
+                String cur = s.substring(i, i + n);
+                if (helper(cur, p)) res.add(i);
+            }
+            return res;
+        }
+
+        public boolean helper(String a, String b) {
+            if (a == null || b == null || a.length() != b.length()) return false;
+            int[] dict = new int[26];
+            for (int i = 0; i < a.length(); i++) {
+                char ch = a.charAt(i);
+                dict[ch - 'a']++;
+            }
+            for (int i = 0; i < b.length(); i++) {
+                char ch = b.charAt(i);
+                dict[ch - 'a']--;
+                if (dict[ch - 'a'] < 0) return false;
+            }
+            return true;
+        }
+    */
+
 
 }
 
